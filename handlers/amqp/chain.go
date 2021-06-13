39ff941ea43631a2cb76f9d1f195463e52b5e25b
egsam98 <egsam98@gmail.com>
2021-06-13 21:38:43 +0300

    init

---
+++ handlers/amqp/chain.go
@@ -0,0 +1,90 @@
+package amqp
+
+import (
+	"time"
+
+	"github.com/Shopify/sarama"
+	votingpb "github.com/egsam98/voting/proto"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/protobuf/proto"
+)
+
+type ChainHandlerOption func(*ChainHandler)
+
+// WithTopicDead treats topic using dead letter queue pattern with provided consumption interval
+func WithTopicDead(consumptionInterval time.Duration) ChainHandlerOption {
+	return func(handler *ChainHandler) {
+		handler.isTopicDead = true
+		handler.consumptionInterval = consumptionInterval
+	}
+}
+
+// ChainHandler is sarama consumer's handler to register vote into blockchain
+type ChainHandler struct {
+	isTopicDead         bool
+	consumptionInterval time.Duration
+	producer            sarama.SyncProducer
+}
+
+func NewChainHandler(
+	producer sarama.SyncProducer,
+	options ...ChainHandlerOption,
+) *ChainHandler {
+	h := &ChainHandler{producer: producer}
+
+	for _, option := range options {
+		option(h)
+	}
+
+	return h
+}
+
+func (c *ChainHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for msg := range claim.Messages() {
+		vote := &votingpb.Vote{}
+		if err := proto.Unmarshal(msg.Value, vote); err != nil {
+			return errors.Wrapf(err, "failed to unmarshal data=%s", string(msg.Value))
+		}
+
+		log.Debug().
+			Str("topic", msg.Topic).
+			Int32("partition", msg.Partition).
+			Int64("offset", msg.Offset).
+			Interface("vote", vote).
+			Msg("handlers.amqp: Received message")
+
+		//if err := c.service.Run(session.Context(), vote); err != nil {
+		//	if c.isTopicDead {
+		//		time.Sleep(c.consumptionInterval)
+		//		return err
+		//	}
+		//
+		//	topicDead := msg.Topic + ".dead"
+		//	if _, _, err := c.producer.SendMessage(&sarama.ProducerMessage{
+		//		Topic: topicDead,
+		//		Value: sarama.ByteEncoder(msg.Value),
+		//	}); err != nil {
+		//		return errors.Wrapf(err, "failed to send message to topic %q", topicDead)
+		//	}
+		//
+		//	log.Error().Stack().Err(err).Msg("handlers.amqp: Vote handling error")
+		//	return nil
+		//}
+
+		if c.isTopicDead {
+			time.Sleep(c.consumptionInterval)
+		}
+
+		session.MarkMessage(msg, "")
+	}
+	return nil
+}
+
+func (*ChainHandler) Setup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (*ChainHandler) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
+
+	"github.com/egsam98/voting/chain/handlers/amqp"
+)
+
+var envs struct {
+	Kafka struct {
+		Addr  string `envconfig:"KAFKA_ADDR"`
+		Topic struct {
+			IsDead bool   `envconfig:"KAFKA_TOPIC_IS_DEAD"`
+			Name   string `envconfig:"KAFKA_TOPIC_NAME"`
+		}
+		Consumer struct {
+			GroupID             string        `envconfig:"KAFKA_CONSUMER_GROUP_ID" required:"true"`
+			ConsumptionInterval time.Duration `envconfig:"KAFKA_CONSUMER_CONSUMPTION_INTERVAL" default:"10s"`
+		}
+	}
+}
+
+func main() {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	if err := run(); err != nil {
+		log.Fatal().Stack().Err(err).Msg("main: Fatal error")
+	}
+}
+
+func run() error {
+	if err := godotenv.Load(); err != nil && !os.IsNotExist(err) {
+		log.Warn().Err(err).Msg("main: Read ENVs from .env file")
+	}
+	if err := envconfig.Process("", &envs); err != nil {
+		return errors.Wrap(err, "failed to parse ENVs to struct")
+	}
+
+	log.Info().
+		Interface("envs", envs).
+		Msg("main: ENVs")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	closeConsumer, err := startConsumer(ctx)
+	if err != nil {
+		return err
+	}
+
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, syscall.SIGINT)
+
+	sig := <-sigint
+	log.Info().Msgf("main: Waiting consumer group %q to complete", envs.Kafka.Consumer.GroupID)
+	cancel()
+	if err := closeConsumer(); err != nil {
+		return err
+	}
+	log.Info().Msgf("main: Terminated via signal %q", sig)
+	return nil
+}
+
+// startConsumer selects consumer with type based on "KAFKA_TOPIC_IS_DEAD" env value and starts listening
+func startConsumer(ctx context.Context) (func() error, error) {
+	cfg := sarama.NewConfig()
+	cfg.Producer.Return.Errors = true
+	cfg.Consumer.Return.Errors = true
+	cfg.Consumer.Offsets.Initial = sarama.OffsetOldest
+
+	kafkaClient, err := sarama.NewClient([]string{envs.Kafka.Addr}, cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to connect to Kafka broker")
+	}
+
+	producer, err := sarama.NewSyncProducerFromClient(kafkaClient)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to init Kafka producer")
+	}
+
+	var options []amqp.ChainHandlerOption
+	if envs.Kafka.Topic.IsDead {
+		options = append(options, amqp.WithTopicDead(envs.Kafka.Consumer.ConsumptionInterval))
+	}
+
+	chainHandler := amqp.NewChainHandler(
+		producer,
+		options...,
+	)
+
+	consumerGroup, err := sarama.NewConsumerGroupFromClient(envs.Kafka.Consumer.GroupID, kafkaClient)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to init Kafka consumer group %q", envs.Kafka.Consumer.GroupID)
+	}
+
+	go func() {
+		for err := range consumerGroup.Errors() {
+			log.Error().Stack().Err(err).Msg("main: Consumer error")
+		}
+	}()
+
+	go func() {
+		format := "main: Consuming from topic=%s, group ID=%s"
+		if envs.Kafka.Topic.IsDead {
+			format = "main: Consuming from dead topic=%s, group ID=%s"
+		}
+		log.Info().Msgf(format, envs.Kafka.Topic.Name, envs.Kafka.Consumer.GroupID)
+
+		for {
+			if err := consumerGroup.Consume(
+				ctx,
+				[]string{envs.Kafka.Topic.Name},
+				chainHandler,
+			); err != nil {
+				log.Fatal().Err(err).Msgf("main: Failed to consume from topic=%s", envs.Kafka.Topic.Name)
+			}
+
+			if ctx.Err() != nil {
+				return
+			}
+		}
+	}()
+
+	return func() error {
+		if err := producer.Close(); err != nil {
+			return errors.Wrap(err, "failed to close Kafka producer")
+		}
+		if err := consumerGroup.Close(); err != nil {
+			return errors.Wrapf(err, "failed to close consumer group %q", envs.Kafka.Consumer.GroupID)
+		}
+		return nil
+	}, nil
+}
